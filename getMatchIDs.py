862eb440c003bccf07cdaace6f0c17e6aa83d134
yeong jinzhi <yeong_jz@hotmail.com>
2018-09-12 23:32:39 +0800

    updated some stuff

--- getMatchIDs.py
+++ getMatchIDs.py
@@ -4,13 +4,17 @@ import pandas as pd
 import csv
 import time
 
-with open('match_id.csv', 'a', encoding='utf-8', newline='') as csvfile:
+
+# open match id csv file in write mode
+with open('match_id.csv', 'w', encoding='utf-8', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
-    for i in range(1,500):
+    for i in range(0,100):
+        # call opendota's public match api(gives 100 randomly sampled match ids)
         r = requests.get("https://api.opendota.com/api/publicMatches")
         data = r.json()
         for j in data:
             try:
+                # write the match id into the csv file
                 writer.writerow([str(j["match_id"])])
             except TypeError:
                 continue
--- getPlayerData.py
+++ getPlayerData.py
@@ -5,29 +5,35 @@ import time
 
 with open('match_id.csv', 'r') as csvfile, open('playerData.json', 'w') as file:
     reader = csv.reader(csvfile, delimiter=',')
-    matchCount = 0
+    matchCount = 1
     errorCount = 0
     for i in reader:
+        # payload is the match id read from match id csv
         payload = i[0]
         url = "https://api.opendota.com/api/matches/" + payload
         try:
+            # attempt to call their api with the match id
             requestMatchData = requests.get(url).json()
         except json.decoder.JSONDecodeError:
+            # dont want the script to stop running because of the below error so
+            # wrapped it in a try except
             print("JSON decode error.")
             errorCount +=1
             continue
         print("Writing match", i,"to file.")
-        count = 1
+        print(matchCount,"records written.")
         try:
+            # sometimes might encounter multiple instances of this data
             for j in requestMatchData["players"]:
-                print("Player",count)
+                # write to json file the player data from the match
                 json.dump(j, file)
-                count+=1
+                file.write('\n')
         except KeyError:
+            # same as above, wrapped in try except to avoid stopping the script
             print("Key error.")
             errorCount+=1
             continue
-        time.sleep(1.1)
         matchCount +=1
-    print(matchCount,"matches written to file.")
+        time.sleep(1.1)
+        
     print(errorCount,"errors.")
