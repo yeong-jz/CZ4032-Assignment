b58cec0f524cd9e75b5b407ee1834d63d9100480
yeong jinzhi <yeong_jz@hotmail.com>
2018-09-19 03:58:14 +0800

    change open mode from write to append

--- filterPlayerData.py
+++ filterPlayerData.py
@@ -2,7 +2,7 @@ import json
 import re
 
 # purpose is to filter labelled and unlabelled player data
-with open('playerData.json', 'r') as file, open('finalPlayerData.json', 'w') as newfile:
+with open('playerData.json', 'r') as file, open('finalPlayerData.json', 'a') as newfile:
     count = 0
     errorCount = 0
     try:
