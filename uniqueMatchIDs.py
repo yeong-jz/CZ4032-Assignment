e25a962e4b236b30b6c48e0f35a2f67c8068f0cc
Neo Hui Lin <huilin10@hotmail.com>
2018-09-20 15:53:51 +0800

    new file added with some data

--- uniqueMatchIDs.py
+++ uniqueMatchIDs.py
@@ -39,3 +39,22 @@ def uniqueMatchIDs():
                 count3+=1
                 writer.writerow(i)
     print("New total :",count3+count2)
+
+    # after checking then we write to file
+    with open("unique_match_id2.csv", "a", newline='') as newfile:
+        writer = csv.writer(newfile, delimiter=',')
+        for i in uniqueIDs:
+            if i not in uniqueIDs2:
+                writer.writerow(i)
+
+    # count new file
+    with open("unique_match_id2.csv", "r", newline='') as newfile:
+        reader2 = csv.reader(newfile, delimiter=',')
+        uniqueIDs2 = []
+        count4 = 0
+        for i in reader2:
+            if i not in uniqueIDs2:
+                uniqueIDs2.append(i)
+        for i in uniqueIDs2:
+            count4+=1
+        print("No. of existing unique match IDs in new file :", count4)
