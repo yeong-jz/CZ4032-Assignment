7420c8c6e89c3177abff940b8bf11e07fcab4a81
LAPTOP-IIU85VUU\cyzse <chengyangzhen@gmail.com>
2018-09-20 18:04:38 +0800

    update

--- getMatchIDs.py
+++ getMatchIDs.py
@@ -8,18 +8,21 @@ import uniqueMatchIDs as umd
 # open match id csv file in write mode
 with open('match_id.csv', 'w', encoding='utf-8', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
-    for i in range(0,100):
-        # call opendota's public match api(gives 100 randomly sampled match ids)
-        r = requests.get("https://api.opendota.com/api/publicMatches")
-        data = r.json()
-        for j in data:
-            try:
-                # write the match id into the csv file
-                writer.writerow([str(j["match_id"])])
-            except TypeError:
-                continue
-        print("Record", i, "added.")
-        time.sleep(1.1)
+    while(True):
+        for i in range(0,100):
+            # call opendota's public match api(gives 100 randomly sampled match ids)
+            r = requests.get("https://api.opendota.com/api/publicMatches")
+            data = r.json()
+            for j in data:
+                try:
+                    # write the match id into the csv file
+                    writer.writerow([str(j["match_id"])])
+                except TypeError:
+                    continue
+            print("Record", i, "added.")
+            time.sleep(1.1)
+        time.sleep(900)
+    
 
 umd.uniqueMatchIDs()
 
